26b7f02966eb8e180848c34ca0527c9c12bbbb33
Nick Palermo <nicholas.palermo8@gmail.com>
2022-03-28 12:47:22 -0400

    Up to Part 30

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 import { AuthService } from './auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -9,14 +9,34 @@ import { AuthService } from './auth.service';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  isLoading = false;
+  isLogin = true;
   constructor(private authService: AuthService, private router: Router) {}
-
   ngOnInit() {}
-
   onLogin() {
+    this.isLoading = true;
     this.authService.login();
     //after login, direct to discover
-    this.router.navigateByUrl('/places/tabs/discover');
+    setTimeout( () => {
+      this.isLoading = false;
+      this.router.navigateByUrl('/places/tabs/discover');
+    }, 1500);
+  }
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
+  }
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+    if (this.isLogin) {
+      //send a request to login serves
+    } else {
+      //send a request to signup servers
+    }
   }
 
 }
--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -10,11 +10,13 @@ import { Place } from '../place.model';
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
+  theFirstItem: Place[];
 
   constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.theFirstItem = this.loadedPlaces.slice(1);
   }
 
 }
